be9438d5921f8ee66d30f5dac24d0f1878666350
one62 <onemo@kakao.com>
2024-01-10 14:35:43 +0900

    first commit

---
+++ src/main/java/b612/bicyclecommunity/BicycleCommunityApplication.java
@@ -0,0 +1,13 @@
+package b612.bicyclecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b612/bicyclecommunity/BicycleCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package b612.bicyclecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
