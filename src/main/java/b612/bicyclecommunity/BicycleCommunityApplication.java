fe1345e6de1d85c4d9ecfd9198883d9da84b05cf
one62 <onemo@kakao.com>
2024-01-29 11:23:50 +0900

    login-demo stash

--- src/main/java/b612/bicyclecommunity/BicycleCommunityApplication.java
+++ src/main/java/b612/bicyclecommunity/BicycleCommunityApplication.java
@@ -2,8 +2,10 @@ package b612.bicyclecommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BicycleCommunityApplication {
 
 	public static void main(String[] args) {
